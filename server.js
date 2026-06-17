435d6c114f357caf07fa91fb55b20cc569776fe6
Rithika S <rithika.s083@gmail.com>
2026-06-17 19:27:39 +0530

    Implement user registration with password hashing

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/", userRoutes);
+app.use("/api/auth", authRoutes);
 
 
 mongoose.connect("mongodb://localhost:27017/mydb")

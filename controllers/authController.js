f4efc970b87c647eb6450c68f7e3f1b69eadd355
Rithika S <rithika.s083@gmail.com>
2026-06-18 17:31:58 +0530

    Add user profile API

--- controllers/authController.js
+++ controllers/authController.js
@@ -77,4 +77,17 @@ const loginUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser , loginUser };
+
+const getProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+module.exports = { registerUser , loginUser , getProfile };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+
+const { protect } = require("../middleware/authMiddleware");
+
 const {
     registerUser,
-    loginUser
+    loginUser,
+    getProfile
 } = require("../controllers/authController");
 
+router.get("/profile", protect, getProfile);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
 
 module.exports = router;

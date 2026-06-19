f7dcccc0491a4c80f1d8133d9e21c881f9d209b1
Rithika S <rithika.s083@gmail.com>
2026-06-19 22:32:53 +0530

    Frontend registration integration progress

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,69 @@
+import { useState } from "react";
+import API from "../services/api";
+
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post("/register", {
+        name,
+        email,
+        password,
+      });
+
+      alert(res.data.message);
+    } 
+    catch (error) {
+      console.log(error);
+      console.log(error.response);
+
+      alert(
+        error.response?.data?.message ||
+        error.message ||
+        "Registration failed"
+      );
+    }
+  };
+
   return (
     <div>
       <h1>Register Page</h1>
+
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const express = require("express");
 require("dotenv").config();
 
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 
 app.use(express.json());
 
